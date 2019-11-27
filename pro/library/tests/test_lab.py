71962ac1ef52ac1bb25f30c3f61f062a6663208b
raevbin <raevbin@gmail.com>
2019-11-27 02:20:40 +0100

    delete unnecessary comments

--- pro/library/tests/test_lab.py
+++ pro/library/tests/test_lab.py
@@ -4,7 +4,6 @@ import urllib
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-# from myproject.apps.core.models import Account
 
 
 class LibraryTests(APITestCase):
